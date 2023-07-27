44d815dfff9001cb9f08c6a4f0ff2e10fc8bfca0
dpadilla90 <d_padilla.y@hotmail.com>
2023-07-26 20:35:36 -0400

    added entity framework core migrations

--- TodoApp-MVC/Controllers/ToDoController.cs
+++ TodoApp-MVC/Controllers/ToDoController.cs
@@ -1,29 +1,59 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TodoApp.Data;
 using TodoApp.Models;
 
 namespace TodoApp.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ToDoController : ControllerBase
+    public class ToDoController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ToDoController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public ToDoController(ApplicationDbContext context)
+        public ToDoController(ILogger<ToDoController> logger, ApplicationDbContext context)
         {
-            _context = context;
+            _logger = logger;
+            _db = context;
+        }
+
+        public IActionResult Index()
+        {
+            var todos = _db.ToDos.Where(todo => !todo.IsCompleted).ToList();
+            return View(todos);
         }
 
         [HttpGet]
-        public IEnumerable<ToDo> Get()
+        public IActionResult AddToDo()
         {
-            return _context.ToDos.Where(todo => !todo.IsCompleted).ToList();
+            return View();
         }
 
-        // Implement other CRUD operations as needed (POST, PUT, DELETE)...
+        [HttpPost]
+        public IActionResult AddToDo(ToDo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                todo.CompletionDate = DateTime.Now; // Set the current date as the default completion date
+                _db.ToDos.Add(todo);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todo);
+        }
+
+        [HttpGet]
+        public IActionResult MarkAsCompleted(int id)
+        {
+            var todo = _db.ToDos.Find(id);
+            if (todo != null)
+            {
+                todo.IsCompleted = true;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ TodoApp-MVC/Migrations/20230727003204_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp_MVC.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    CompletionDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    IsCompleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
